572dccd7c8250241e3bca5f04b9db628bc19bd28
renox <planetrenox@protonmail.com>
2024-02-25 03:09:47 -0800

    new style

--- readme.md
+++ readme.md
@@ -1,40 +1,103 @@
 # cute-dynamo
 
-
-aws shorthands
-
+Make DynamoDB cute with minimalism; worry about network costs later.
 
 ## Features
 
-
-```JavaScript
-export async function createDynamoDBClient({ region, identityPoolId, accessKeyId, secretAccessKey })
-```
-
+- Fast development.
+- Simple syntax.
+- No pain.
+- I didn't test if this works yet
+- Uses 'latest' aws packages ALWAYS.
 
 ## Why
 
+`cute-dynamo` solves a very fundamental problem with DynamoDB: and that is, development is **slow**...   
+With this design, we define rules that allow you to use DynamoDB across **different projects** without having **custom** code for each project.
 
-aws shorthands
+Cute tables are **minimalistic** and only allow **1 attribute per item** which is a serialized **JSON** object.
 
+By design of DynamoDB, each item is limited to 400kb in size no matter how many attributes anyway, so we aren't limiting ourselves.
+
+Usage:   
+**Serverside** with AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESS_KEY   
+**Clientside** with AWS_IDENTITY_POOL_ID
+
+Cute tables **must _adhere_ to naming**: [Primary key name: PK], [Sort key name: SK], [Single attribute name: JSON]
+
+> ###### **There can't be cute without development speed.**
 
 ## Getting Started
 
-
 ### Installation
-
 
    ```
    npm install cute-dynamo
-   npm install --global cute-dynamo
    ```
-
 
 ### Usage
 
+   ```javascript
+// Initialization
+//
+// First, ensure you initialize the DynamoDB client. 
+// Depending on your application context (server-side or client-side),
+// you have two options for initialization:
+    import { init } from 'cute-dynamo'; // (process.env.DYNAMODB_TABLE)
+    await init(); // look in code for env vars naming. 
 
+// Server-Side Initialization (using AWS Access Key ID and AWS Secret Access Key):
+// IF YOU DONT WANT TO USE ENV VARS
+//     await init({
+//         region: 'us-east-1',
+//         accessKeyId: 'AKIAEXAMPLE',
+//         secretAccessKey: 'secret'
+//     });
+
+// Initialize with Cognito Identity Pool ID for client-side usage
+// IF YOU DONT WANT TO USE ENV VARS
+//     await init({
+//         region: 'us-east-1',
+//         identityPoolId: 'us-east-1:exampleId'
+//     });
+
+// Putting Items into DynamoDB
+    import { put } from 'cute-dynamo';
+// To store an item in DynamoDB, you can use the put function. This function requires you to 
+// provide JS object you wish to store, along with a primary key (pk) and an optional sort key (sk).
+
+// Storing an item with only a primary key:
+    await put({name: 'John Doe', age: 30}, 'USER#001');
+
+// Storing an item with both a primary key and a sort key:
+    await put({lastLogin: '2024-02-25T08:00:00Z'}, 'USER#001', 'METADATA#001');
+
+// Getting Items from DynamoDB
+    import { get } from 'cute-dynamo';
+
+// Retrieving an item using only a primary key:
+    const item = await get('USER#001');
+    console.log(item);
+
+// Retrieving an item using both a primary key and a sort key:
+    const item = await get('USER#001', 'METADATA#001');
+    console.log(item);
+
+// These examples cover the basic operations to get you started with cute-dynamo. 
+// By following the minimalistic design principles of cute-dynamo, you 
+// can ensure a consistent and simplified approach to working with DynamoDB across your projects.
    ```
 
+### FYI
 
+| DynamoDB Call                                   | Estimated Time (ms) | Explanation                                                                                        |
+|-------------------------------------------------|---------------------|----------------------------------------------------------------------------------------------------|
+| PK lookup in PK-only table                      | 5 - 50              | Direct `GetItem` access with only a PK. Fast and efficient.                                        |
+| **PK lookup in PK-SK table**                    | **N/A**             | **Direct lookup not possible without SK. Use `Query` instead.**                                    |
+| PK and SK lookup in PK-SK table               | 5 - 50               | Direct `GetItem` access with both PK and SK. Very fast and efficient for accessing a single item.    |
+| PK `Query` in PK-SK table (single item)         | 10 - 100            | `Query` based on PK, fast if there's only one item for the PK.                                     |
+| PK `Query` in PK-SK table (multiple items)      | 20 - 200            | `Query` based on PK, might take longer depending on the number of items returned.                  |
+| Full Scan on any table (small table <1k items)  | 100 - 1000          | Scans are slower and more resource-intensive, time increases with item size and count.             |
+| Full Scan on any table (large table >10k items) | 1000 - 5000+        | Significantly slower, highly dependent on table size, item size, and scan configuration.           |
+| Conditional `Query` on PK-SK table (filters)    | 20 - 200            | Using `Query` with additional filters can increase time, depending on complexity and results size. |
 
-   ```
--- src/index.js
+++ src/index.js
@@ -1,133 +1,124 @@
-import {
-    DeleteCommand,
-    GetCommand,
-    UpdateCommand,
-    QueryCommand,
-    ScanCommand,
-} from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-export async function createDynamoDBDocumentClient({region, identityPoolId, accessKeyId, secretAccessKey})
+// cute-dynamo solves a very fundamental problem with DynamoDB: and that is, development is slow...
+// With this design, we define rules that allow you to use DynamoDB across different projects without having custom code for each project.
+// Cute tables are minimalistic and are only allowed 1 attribute per item which is a serialized JSON object, so you don't need to worry about weird dynamodb syntax.
+// By design of DynamoDB, each item is limited to 400kb in size no matter how many attributes anyway, so we aren't limiting ourselves.
+// Usage serverside or clientside: (serverside with process.env.AWS_ACCESS_KEY_ID & process.env.AWS_SECRET_ACCESS_KEY) or (clientside with process.env.AWS_IDENTITY_POOL_ID)
+// Cute tables must adhere to naming: [Primary key name: PK], [Sort key name: SK], [Single attribute name: JSON]
+
+let client;
+/**
+ * Initializes the DynamoDB client for cute-dynamo library usage. It configures the client based on the environment and credentials provided.
+ * This function supports two authentication methods:
+ * 1. Client-side authentication using an AWS Identity Pool ID for applications where AWS credentials are managed client-side.
+ * 2. Server-side authentication using AWS Access Key ID and AWS Secret Access Key for server-side applications.
+ *
+ * By adhering to cute-dynamo's minimalistic design, this initialization sets the stage for interacting with DynamoDB tables that conform to the specified naming conventions and data structure rules.
+ *
+ * @param {Object} options - Configuration options for the DynamoDB client. Includes region, identityPoolId, accessKeyId, and secretAccessKey.
+ * @param {string} [options.region=process.env.AWS_REGION] - The AWS region where the DynamoDB instance is hosted.
+ * @param {string} [options.identityPoolId=process.env.AWS_IDENTITY_POOL_ID] - The AWS Cognito Identity Pool ID for client-side authentication. Optional if server-side credentials are provided.
+ * @param {string} [options.accessKeyId=process.env.AWS_ACCESS_KEY_ID] - The AWS Access Key ID for server-side authentication. Required if not using client-side authentication with an Identity Pool ID.
+ * @param {string} [options.secretAccessKey=process.env.AWS_SECRET_ACCESS_KEY] - The AWS Secret Access Key for server-side authentication. Required if not using client-side authentication.
+ *
+ * @throws {Error} Throws an error if insufficient credentials are provided for either authentication method.
+ *
+ * @example
+ * // Initialize for client-side usage with an identity pool ID
+ * await init({ region: 'us-east-1', identityPoolId: 'us-east-1:exampleId' });
+ *
+ * @example
+ * // Initialize for server-side usage with access key and secret key
+ * await init({ region: 'us-east-1', accessKeyId: 'AKIAEXAMPLE', secretAccessKey: 'secret' });
+ *
+ * Note: This setup assumes the environment variables are set for AWS_REGION, AWS_IDENTITY_POOL_ID, AWS_ACCESS_KEY_ID, and AWS_SECRET_ACCESS_KEY as fallbacks.
+ */
+export async function init({region = process.env.AWS_REGION, identityPoolId = process.env.AWS_IDENTITY_POOL_ID, accessKeyId = process.env.AWS_ACCESS_KEY_ID, secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY} = {})
 {
     let credentials;
     if (identityPoolId) {
         const {CognitoIdentityClient} = await import("@aws-sdk/client-cognito-identity");
         const {fromCognitoIdentityPool} = await import("@aws-sdk/credential-provider-cognito-identity");
         credentials = fromCognitoIdentityPool({
-            client: new CognitoIdentityClient({region}),
-            identityPoolId,
+            client: new CognitoIdentityClient({region}), identityPoolId,
         });
     }
-    else if (accessKeyId && secretAccessKey) {
-        credentials = {
-            accessKeyId,
-            secretAccessKey,
-        };
-    }
-    else {
-        throw new Error("Insufficient credentials provided");
-    }
-
-    const {DynamoDBClient} = await import("@aws-sdk/client-dynamodb");
-    const {DynamoDBDocumentClient} = await import("@aws-sdk/lib-dynamodb");
-    let client = new DynamoDBClient({
-        region,
-        credentials,
-    });
-    return DynamoDBDocumentClient.from(client);
+    else if (accessKeyId && secretAccessKey) credentials = {accessKeyId, secretAccessKey}; else throw new Error("Insufficient credentials provided");
+    client = DynamoDBDocumentClient.from(new DynamoDBClient({region, credentials}));
 }
 
-export async function put(client, item)
+/**
+ * Retrieves an item from the DynamoDB table using a primary key and an optional sort key.
+ * The function deserializes the "JSON" attribute of the retrieved item back into a JavaScript object.
+ * The sort key can be a string or a number.
+ *
+ * @param {string} pk - The primary key (PK) of the item to retrieve.
+ * @param {string|number} [sk=undefined] - Optional. The sort key (SK) to further specify the item to retrieve.
+ *
+ * @returns {Promise<Object>} - A promise that resolves to the retrieved item, with the "JSON" attribute deserialized into an object. If no item is found, the promise resolves to null.
+ *
+ * @example
+ * // Retrieve an item using only a primary key
+ * const item = await get('USER#001');
+ * console.log(item); // Outputs: { PK: 'USER#001', JSON: { name: 'John Doe', age: 30 } }
+ *
+ * @example
+ * // Retrieve an item using both a primary key and a numeric sort key
+ * const item = await get('USER#001', 20240225);
+ * console.log(item); // Outputs: { PK: 'USER#001', SK: 20240225, JSON: { lastLogin: '2024-02-25T08:00:00Z' } }
+ */
+
+export async function get(pk, sk = undefined)
+{
+    const {GetCommand} = await import("@aws-sdk/lib-dynamodb");
+    const key = {
+        PK: pk, ...(sk && {SK: sk}), // Include SK in the key if provided
+    };
+
+    const response = await client.send(new GetCommand({
+        TableName: process.env.DYNAMODB_TABLE, Key: key,
+    }));
+
+    // Deserialize the "JSON" attribute if it exists
+    if (response.Item && response.Item.JSON) {
+        response.Item.JSON = JSON.parse(response.Item.JSON);
+    }
+
+    return response.Item ? response.Item : null;
+}
+
+/**
+ * Stores an item in the DynamoDB table with specified data, a primary key, and an optional sort key.
+ * The data object is automatically serialized into a JSON string before storage.
+ * The sort key can be a string or a number, providing additional sorting and query flexibility.
+ *
+ * @param {Object} data - The JavaScript object containing the data to be stored in the "JSON" attribute.
+ * @param {string} pk - The primary key (PK) for the item. Must uniquely identify the item.
+ * @param {string|number} [sk=undefined] - Optional. The sort key (SK) for the item.
+ *
+ * @returns {Promise} - A promise that resolves to the result of the PutCommand operation.
+ *
+ * @example
+ * // Store an item with only a primary key
+ * await put({ name: 'John Doe', age: 30 }, 'USER#001');
+ *
+ * @example
+ * // Store an item with both a primary key and a numeric sort key
+ * await put({ lastLogin: '2024-02-25T08:00:00Z' }, 'USER#001', 20240225);
+ */
+export async function put(data, pk, sk = undefined)
 {
     const {PutCommand} = await import("@aws-sdk/lib-dynamodb");
-    return await client.send(
-        new PutCommand({
-            TableName: process.env.DYNAMODB_TABLE,
-            Item: item,
-        }));
+    const item = {
+        PK: pk, ...(sk && {SK: sk}), // Include SK in the item if provided
+        JSON: JSON.stringify(data),
+    };
+
+    return await client.send(new PutCommand({
+        TableName: process.env.DYNAMODB_TABLE, Item: item,
+    }));
 }
 
-export async function get(client, key) {
-    return await client.send(
-        new GetCommand({
-            TableName: process.env.DYNAMODB_TABLE,
-            Key: key,
-        })
-    );
-}
 
-/**
- * Updates specific attributes of an item in DynamoDB without replacing it.
- * @param {DynamoDBDocumentClient} client DynamoDB Document Client.
- * @param {Object} key Key of the item to update.
- * @param {String} updateExpression Update expression specifying attributes to update.
- * @param {Object} expressionAttributeValues Values referenced in the update expression.
- *
- * Example usage:
- * await updateItem(client, { id: "123" }, "set #name = :name", { ":name": "Jane Doe", "#name": "name" });
- * Note: Update modifies specific attributes, leaving the rest of the item unchanged.
- */
-export async function updateItem(client, key, updateExpression, expressionAttributeValues) {
-    return await client.send(
-        new UpdateCommand({
-            TableName: process.env.DYNAMODB_TABLE,
-            Key: key,
-            UpdateExpression: updateExpression,
-            ExpressionAttributeValues: expressionAttributeValues,
-        })
-    );
-}
-
-/**
- * Deletes an item from DynamoDB.
- * @param {DynamoDBDocumentClient} client DynamoDB Document Client.
- * @param {Object} key Key of the item to delete.
- *
- * Example usage:
- * await deleteItem(client, { id: "123" });
- */
-export async function deleteItem(client, key) {
-    return await client.send(
-        new DeleteCommand({
-            TableName: process.env.DYNAMODB_TABLE,
-            Key: key,
-        })
-    );
-}
-
-/**
- * Queries items based on a key condition expression.
- * @param {DynamoDBDocumentClient} client DynamoDB Document Client.
- * @param {String} keyConditionExpression Condition expression for the query.
- * @param {Object} expressionAttributeValues Values for the condition expression.
- *
- * Example usage:
- * await queryItems(client, "id = :id", { ":id": "123" });
- */
-export async function queryItems(client, keyConditionExpression, expressionAttributeValues) {
-    return await client.send(
-        new QueryCommand({
-            TableName: process.env.DYNAMODB_TABLE,
-            KeyConditionExpression: keyConditionExpression,
-            ExpressionAttributeValues: expressionAttributeValues,
-        })
-    );
-}
-
-/**
- * Scans the table and returns items matching the optional filter expression.
- * @param {DynamoDBDocumentClient} client DynamoDB Document Client.
- * @param {String} [filterExpression] Optional filter expression for the scan.
- * @param {Object} [expressionAttributeValues] Values for the filter expression.
- *
- * Example usage:
- * await scanTable(client, "contains(#name, :name)", { ":name": "Doe", "#name": "name" });
- */
-export async function scanTable(client, filterExpression, expressionAttributeValues) {
-    return await client.send(
-        new ScanCommand({
-            TableName: process.env.DYNAMODB_TABLE,
-            FilterExpression: filterExpression,
-            ExpressionAttributeValues: expressionAttributeValues,
-        })
-    );
-}
